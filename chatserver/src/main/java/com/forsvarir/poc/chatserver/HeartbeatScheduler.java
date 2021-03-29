8b3b56bfa4ac17ccd45eaa832a57bbac0963bead
Mark Loveland-Armour <forsvarir@blueyonder.co.uk>
2021-03-29 20:10:28 +0100

    Added Scheduler example for sending heartbeat messages

---
+++ chatserver/src/main/java/com/forsvarir/poc/chatserver/HeartbeatScheduler.java
@@ -0,0 +1,39 @@
+package com.forsvarir.poc.chatserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class HeartbeatScheduler {
+    @Autowired
+    private ThreadPoolTaskScheduler taskScheduler;
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @PostConstruct
+    public void scheduleHeartbeatMessage() {
+        taskScheduler.scheduleAtFixedRate(new HeartbeatTask("2 second heartbeat", messagingTemplate), 2000);
+    }
+
+    class HeartbeatTask implements Runnable {
+
+        private final String message;
+        private final SimpMessagingTemplate messagingTemplate;
+
+        public HeartbeatTask(String message, SimpMessagingTemplate messagingTemplate) {
+            this.message = message;
+            this.messagingTemplate = messagingTemplate;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Runnable Task with " + message + " on thread " + Thread.currentThread().getName());
+            messagingTemplate.convertAndSend("/topic/greetings", "{\"content\": \"" + message + "\"}");
+        }
+    }
+}
---
+++ chatserver/src/main/java/com/forsvarir/poc/chatserver/ThreadPoolTaskSchedulerConfig.java
@@ -0,0 +1,19 @@
+package com.forsvarir.poc.chatserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+@ComponentScan(basePackages = "com.forsvarir.poc.chatserver", basePackageClasses = {HeartbeatScheduler.class})
+public class ThreadPoolTaskSchedulerConfig {
+
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(2);
+        threadPoolTaskScheduler.setThreadNamePrefix("ThreadPoolTaskScheduler");
+        return threadPoolTaskScheduler;
+    }
+}
